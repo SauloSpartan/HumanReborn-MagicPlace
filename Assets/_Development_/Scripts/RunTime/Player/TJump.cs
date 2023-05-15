5001a3ef57fea18892e298f8e3c139b994127308
Saulo Trujillo <82285645+SauloSpartan@users.noreply.github.com>
2023-05-15 17:57:23 -0600

    Player Triple Jump
    For now I will let the player have triple jump because I am not being able to do a simple double jump without breaking my jump code and making it super complicated

--- Assets/_Development_/Scripts/RunTime/Player/TJump.cs
+++ Assets/_Development_/Scripts/RunTime/Player/TJump.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using UnityEngine;
 
 public class TJump : MonoBehaviour
@@ -7,20 +6,33 @@ public class TJump : MonoBehaviour
 
     [SerializeField] private float _jumpForce;
     [SerializeField] private int _maxJumps;
-    [SerializeField] private float _groundDistance;
-    [SerializeField] private LayerMask _groundLayer;
+    private int _initialJumps;
+    private LayerMask _groundLayer;
 
     void Start()
     {
         _rigidBody = GetComponent<Rigidbody2D>();
+        _initialJumps = _maxJumps;
+        _groundLayer = LayerMask.GetMask("Ground");
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && _maxJumps > 0)
         {
+            _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, 0);
             _rigidBody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
             _maxJumps -= 1;
         }
+
+        if (IsGrounded())
+        {
+            _maxJumps = _initialJumps;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(transform.position, 0.2f, _groundLayer);
     }
 }
