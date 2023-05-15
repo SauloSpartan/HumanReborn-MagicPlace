f7ed8e9f7300137305366b16cfae2ab39a1aa116
Saulo Trujillo <82285645+SauloSpartan@users.noreply.github.com>
2023-05-15 10:17:45 -0600

    Screen Resolution - Options
    I added a dropdown list where you can change the screen resolution of the game.

--- Assets/_Development_/Scripts/RunTime/UI/TMenuManager.cs
+++ Assets/_Development_/Scripts/RunTime/UI/TMenuManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -14,8 +15,10 @@ public class TMenuManager : MonoBehaviour
 
     [SerializeField] private Button _fullScreenButton;
     [SerializeField] private TMP_Text _fullScreenText;
+    [SerializeField] private TMP_Dropdown _screenResolution;
+    private string[] _resolutionSizes = { "800x600", "1024x768", "1152x864", "1280x720", "1280x768", "1280x1024", "1360x768", "1366x768", "1600x900", "1600x1024", "1920x1080" };
 
-    void Awake()
+    void Awake()    
     {
         Screen.fullScreen = true;
     }
@@ -26,6 +29,8 @@ public class TMenuManager : MonoBehaviour
         _optionsButton.onClick.AddListener(OptionsMenu);
         _goBackButton.onClick.AddListener(MainMenu);
         _fullScreenButton.onClick.AddListener(FullScreen);
+        ResolutionDropdown();
+        _screenResolution.onValueChanged.AddListener(delegate {ScreenResolution(); });
     }
 
     private void GameStart()
@@ -56,6 +61,64 @@ public class TMenuManager : MonoBehaviour
         else
         {
             _fullScreenText.text = "!FULL SCREEN";
+        }
+    }
+
+    private void ResolutionDropdown()
+    {
+        foreach (string size in _resolutionSizes)
+        {
+            _screenResolution.options.Add(new TMP_Dropdown.OptionData() { text = size });
+        }
+    }
+
+    private void ScreenResolution()
+    {
+        switch (_resolutionSizes[_screenResolution.value])
+        {
+            case "800x600":
+                Screen.SetResolution(800, 600, Screen.fullScreen, 1);
+                break;
+
+            case "1024x768":
+                Screen.SetResolution(1024, 768, Screen.fullScreen, 1);
+                break;
+
+            case "1152x864":
+                Screen.SetResolution(1152, 864, Screen.fullScreen, 1);
+                break;
+
+            case "1280x720":
+                Screen.SetResolution(1280, 720, Screen.fullScreen, 1);
+                break;
+
+            case "1280x768":
+                Screen.SetResolution(1280, 768, Screen.fullScreen, 1);
+                break;
+
+            case "1280x1024":
+                Screen.SetResolution(1280, 1024, Screen.fullScreen, 1);
+                break;
+
+            case "1360x768":
+                Screen.SetResolution(1360, 768, Screen.fullScreen, 1);
+                break;
+
+            case "1366x768":
+                Screen.SetResolution(1366, 768, Screen.fullScreen, 1);
+                break;
+
+            case "1600x900":
+                Screen.SetResolution(1600, 900, Screen.fullScreen, 1);
+                break;
+
+            case "1600x1024":
+                Screen.SetResolution(1600, 1024, Screen.fullScreen, 1);
+                break;
+
+            case "1920x1080":
+                Screen.SetResolution(1920, 1080, Screen.fullScreen, 1);
+                break;
         }
     }
 }
